0beff7bd55a91f4cb85b8659283daaa5bae93115
DemetrioSantana <familia.santana0207@gmail.com>
2021-09-07 19:31:05 -0300

    Projeto criado para estudar framework react

---
+++ backend/projectvendas/src/main/java/com/projectopen/projectvendas/ProjectvendasApplication.java
@@ -0,0 +1,13 @@
+package com.projectopen.projectvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectvendasApplication.class, args);
+	}
+
+}
