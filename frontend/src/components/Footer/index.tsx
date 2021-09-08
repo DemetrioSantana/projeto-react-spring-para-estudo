090b37dd3d77b4c525922354a5679136e5afd74e
DemetrioSantana <familia.santana0207@gmail.com>
2021-09-07 22:03:17 -0300

    Adicionando os navbar, footer e datatable com dados estáticos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/DemetrioSantana" target="_blank" rel="noreferrer">Demétrio Santana</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/demetrio.santana" target="_blank" rel="noreferrer">@demetrio.santana</a></small></p>
+  </div>
+</footer>
+    );
+  }
+  
+  export default Footer;
+  
