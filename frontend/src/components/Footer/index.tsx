74b66859c795d3dd138361fd7ab292a586842056
DemetrioSantana <familia.santana0207@gmail.com>
2021-09-07 23:59:09 -0300

    Alterando o nome do footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   <div className="container">
     <p className="text-light">App desenvolvido por <a href="https://github.com/DemetrioSantana" target="_blank" rel="noreferrer">Demétrio Santana</a></p>
     <p className="text-light"><small><strong>Semana Spring React</strong><br/>
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/demetrio.santana" target="_blank" rel="noreferrer">@demetrio.santana</a></small></p>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
   </div>
 </footer>
     );
