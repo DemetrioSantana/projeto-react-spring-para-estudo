7d8264573a7d1cea056785e2e61e89aaaf6b59ef
DemetrioSantana <familia.santana0207@gmail.com>
2021-09-07 23:55:56 -0300

    charts estaticos adicionado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
+import Barchat from "components/BarChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
+import DonutChart from "components/DonutChart";
 
 
 function App() {
@@ -8,7 +10,22 @@ function App() {
       <>
       <NavBar />
     <div className="container"> 
-      <h1 className="text-primary">Olá mundo!</h1>
+      <h1 className="text-primary py-3">DashBoard de vendas</h1>
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secundary">Taxa de sucesso (%)</h5>
+          <Barchat />
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secundary">Todas as vendas</h5>
+
+          <DonutChart />
+        </div>
+      </div>
+
+      <div className="py-3">
+        <h2 className="text-primary">Todas as vendas</h2>
+      </div>
       <DataTable />
     </div>
       <Footer />
