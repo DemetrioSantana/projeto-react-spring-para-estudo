9fafc5af925829e97ec37b85311cff428e1cb2f5
DemetrioSantana <familia.santana0207@gmail.com>
2021-09-12 12:18:01 -0300

    Centralizando a pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -8,21 +8,21 @@ type Props = {
 const Pagination = ({ page, onPageChange }: Props) => {
    
     return (
-        <div className="row d-flex justify-content-center">
-    <nav>
-        <ul className="pagination">
-            <li className={`page-item ${page.first ? 'disabled' : ''}`} >
-                <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
-            </li>
-            <li className="page-item disabled">
-                <span className="page-link">{page.number + 1}</span>
-            </li>
-            <li className={`page-item ${page.last ? 'disabled' : ''}`} >
-                <button className="page-link" onClick = {() => onPageChange(page.number + 1)}>Próxima</button>
-            </li>
-        </ul>
-    </nav>
-</div>
+        <div className="d-flex justify-content-center">
+            <nav>
+                <ul className="pagination">
+                    <li className={`page-item ${page.first ? 'disabled' : ''}`} >
+                        <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
+                    </li>
+                    <li className="page-item disabled">
+                        <span className="page-link">{page.number + 1}</span>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''}`} >
+                        <button className="page-link" onClick = {() => onPageChange(page.number + 1)}>Próxima</button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 export default Pagination;
